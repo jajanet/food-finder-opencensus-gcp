86881279a977fca7e578655e3a0e528e162ea9e6
Janet Vu <vjanet@umich.edu>
2020-06-15 18:27:01 +0000

    Reduce random fail chance and add timeout error

--- include/simulatedProcessing.cc
+++ include/simulatedProcessing.cc
@@ -7,14 +7,21 @@
 #include "absl/time/time.h"
 #include "absl/time/clock.h"
 
-void doDelay() { // add delay from .1 to .6 sec
-  int delay = rand() % 500 + 100;
-  absl::SleepFor(absl::Milliseconds(delay));
+void doTimeout() { // timeout
+  absl::sleepfor(absl::milliseconds(9999999));
+}
+
+
+
+void doDelay() { // add delay from up to .5 sec
+  int delay = rand() % 500;
+  if (delay < 10) doTimeout();
+  absl::sleepfor(absl::milliseconds(delay));
 }
 
 
 grpc::Status randomlyFailedStatus() {
-  return rand() % 6 != 0?
+  return rand() % 12 != 0?
     grpc::Status::OK
     : grpc::Status(grpc::StatusCode::ABORTED, "Random fail");
 }
