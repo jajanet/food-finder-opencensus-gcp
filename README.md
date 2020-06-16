3c81a8dce8118c6f9b9b597a3eec6968982c33e7
Janet Vu <vjanet@umich.edu>
2020-06-16 13:36:30 +0000

    Update run instructions to be easier

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# Summary
+Search for a ingredients to find available vendors and the ingredient's remaining stock and current price at those vendors. The ingredient and vendor information is currently stored metadata at their respective servers.
+
+Available ingredients to search for (comma separated): yeast, flour, sugar, noodles, chocolate, salt
+
+
+This Project that demostrates usage of:
+- RCP server-client communication (gRPC)
+- Tracing and monitoring (OpenCensus)
+- Exporting tracing and monitoring (Stackdriver to Google Cloud Platoform) 
+
+
+# Prerequisites
+See other at the bottom of the README for installation needs.
+
+To see data exported to Google Cloud Platform, set your project ID in include/exporters.cc. Otherwise, stats and tracing only shows in terminal
+
+
 # To Build:
 `bazel build :all`
 
--- include/exporters.cc
+++ include/exporters.cc
@@ -8,34 +8,27 @@
 
 #include "opencensus/exporters/stats/stackdriver/stackdriver_exporter.h"
 #include "opencensus/exporters/stats/stdout/stdout_exporter.h"
-#include "opencensus/exporters/trace/ocagent/ocagent_exporter.h"
 #include "opencensus/exporters/trace/stackdriver/stackdriver_exporter.h"
 #include "opencensus/exporters/trace/stdout/stdout_exporter.h"
 
-
+// Only exporting to Google Cloud Platform using Stackdriver
 void RegisterExporters() {
   // For debugging, register exporters that just write to stdout.
   opencensus::exporters::stats::StdoutExporter::Register();
   opencensus::exporters::trace::StdoutExporter::Register();
+  
+  // SET YOUR POROJECT ID HERE
+  const char* project_id = "starter-project-j";
 
-  const char* project_id = getenv("STACKDRIVER_PROJECT_ID");
-  if (project_id == nullptr) {
-    std::cerr << "The STACKDRIVER_PROJECT_ID environment variable is not set: "
-                 "not exporting to Stackdriver.\n";
-  } else {
-    std::cout << "RegisterStackdriverExporters:\n";
-    std::cout << "  project_id = \"" << project_id << "\"\n";
+  opencensus::exporters::stats::StackdriverOptions stats_opts;
+  stats_opts.project_id = project_id;
+  opencensus::exporters::stats::StackdriverExporter::Register(
+      std::move(stats_opts));
 
-    opencensus::exporters::stats::StackdriverOptions stats_opts;
-    stats_opts.project_id = project_id;
-    opencensus::exporters::stats::StackdriverExporter::Register(
-        std::move(stats_opts));
-
-    opencensus::exporters::trace::StackdriverOptions trace_opts;
-    trace_opts.project_id = project_id;
-    opencensus::exporters::trace::StackdriverExporter::Register(
-        std::move(trace_opts));
-  }
+  opencensus::exporters::trace::StackdriverOptions trace_opts;
+  trace_opts.project_id = project_id;
+  opencensus::exporters::trace::StackdriverExporter::Register(
+      std::move(trace_opts));
 
 }
 
